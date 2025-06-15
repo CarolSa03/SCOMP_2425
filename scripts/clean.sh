fe17518028dd981481bc8b45915fce52f5159499
Carolina Sá <117090772+CarolSa03@users.noreply.github.com>
2025-06-15 17:16:22 +0100

    script updated

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rm -f main.o thread.o drone.o drone simulation_report.txt
 rm -f /dev/shm/drone_sim /dev/shm/sem_step /dev/shm/sem_collision /dev/shm/sem.sem_step_ready /dev/shm/sem.sem_step_continue
